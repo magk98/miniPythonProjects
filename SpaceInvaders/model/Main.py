26fe04ed878ae739fbcb2830ed47a0ca42f11cce
magk98 <43295061+magk98@users.noreply.github.com>
2020-02-18 19:05:12 +0100

    v1.0

--- SpaceInvaders/model/Main.py
+++ SpaceInvaders/model/Main.py
@@ -1,26 +1,34 @@
+import random
+
 import pygame
-import os
+
+from model import Level
+from model.Character import Character
 from model.Ship import Ship
 
-#window init
-
-pygame.init()
-SCREEN_WIDTH, SCREEN_HEIGHT = 300, 200
+#game variables
+SCREEN_WIDTH, SCREEN_HEIGHT = 600, 400
 dark_sky = (14, 15, 70)
+red = (255, 10, 10)
+green = (10, 255, 10)
+#window init
+pygame.init()
 win = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Space Invaders')
 win.fill(dark_sky)
+font = pygame.font.Font('freesansbold.ttf', 32)
 
-#images load
-#alien = pygame.image.load(os.path.join('images', 'alien.png'))
+
 is_running = True
+alien_left_move = False
 
 
 def main():
-    global is_running
+    global is_running, alien_left_move
     ship = Ship(SCREEN_WIDTH//2, SCREEN_HEIGHT - 30, 10, 10)
-    ship.draw(win)
+    ship.draw()
     shot_counter = 0
+    level = Level.Level(1)
     while is_running:
         pygame.time.delay(40)
         shot_counter += 1
@@ -38,13 +46,24 @@ def main():
             ship.move(0, -10)
         if keys[pygame.K_DOWN]:
             ship.move(0, 10)
-        ship.draw(win)
 
-        if shot_counter % 10 == 0:
-            ship.shot(win)
-        for shot in ship.shots:
-            shot.draw(win, ship)
-            shot.move()
+        level.draw_components(ship)
+
+        if level.alive_aliens:
+            if shot_counter % 10 == 0:
+                ship.shot()
+            if shot_counter % 20 == 5:
+                level.move_aliens(alien_left_move, False)
+                alien_left_move = not alien_left_move
+            if shot_counter % 40 == 20:
+                alien = random.choice(level.alive_aliens)
+                level.alien_shots += [Character.Shot(alien.x + SCREEN_WIDTH // 40, alien.y + SCREEN_HEIGHT // 30)]
+            if shot_counter % 50 == 0:
+                level.move_aliens(False, True)
+        if not level.alive_aliens:
+            level = Level.Level(level.number + 1, level.score)
+
+        level.check_collision(ship)
 
     pygame.quit()
 
--- SpaceInvaders/model/Ship.py
+++ SpaceInvaders/model/Ship.py
@@ -1,39 +1,25 @@
-import pygame, os
+import os
 
+import pygame
+
+from model import Main
 from model.Character import Character
-
-dark_sky = (14, 15, 70)
-red = (255, 10, 10)
-SCREEN_WIDTH, SCREEN_HEIGHT = 300, 200
 
 
 class Ship(Character):
     def __init__(self, _x, _y, _speed, _gun_speed):
         super(Ship, self).__init__(_x, _y, _speed, _gun_speed)
-        self.image_name = 'shiip.png'
+        self.image_name = 'ship.png'
         self.shots = []
+        self.health = 3
 
-    def shot(self, win):
-        self.shots += [Ship.Shot(self.x + 8, self.y)]
-        #pygame.draw.rect(win, red, (x, y, 4, 8))
-        pygame.display.flip()
-        self.draw(win)
-        win.fill(dark_sky)
+    def shot(self):
+        self.shots += [Character.Shot(self.x + Main.SCREEN_WIDTH // 40, self.y)]
+        self.draw()
 
-    class Shot:
-        def __init__(self, _x, _y):
-            self.x = _x
-            self.y = _y
-
-        def draw(self, win, ship):
-            pygame.draw.rect(win, red, (self.x - 1, self.y, 4, 8))
-            if self.y > SCREEN_HEIGHT:
-                ship.shots.remove(self)
-
-        def move(self):
-            global SCREEN_HEIGHT, SCREEN_WIDTH
-            x_diff, y_diff = 0, -8
-            self.x += x_diff
-            self.y += y_diff
-
+    def draw_health(self):
+        image = pygame.image.load(os.path.join('images', self.image_name))
+        image = pygame.transform.scale(image, (Main.SCREEN_WIDTH // 40, Main.SCREEN_HEIGHT // 40)).convert_alpha()
+        for i in range(0, self.health):
+            Main.win.blit(image, (Main.SCREEN_WIDTH - (i + 2) * (Main.SCREEN_WIDTH // 40), 10))
 
